b96ac3628a38d4508f6c68b9d49592191c0befcc
Vladislav Melnik <vladislav.melnik@simplestaking.com>
2020-11-19 11:38:48 +0000

    add bridge

--- packet_generator.sh
+++ packet_generator.sh
@@ -1,21 +1,30 @@
 #!/usr/bin/env bash
 
 interface=tft
+bridge=br0
 
+function cleanup {
+    sudo ip tuntap del mode tap $interface
+    sudo ip link del $bridge type bridge
+}
+
+sudo ip link add $bridge type bridge
 sudo ip tuntap add mode tap $interface
+sudo ip link set $interface master $bridge
+sudo ip link set enp4s0 master $bridge
+
 sudo ip addr add "192.168.1.1/24" dev $interface
+sudo ip link set $interface up
+
 sleep .1
 cargo run -p packet-generator -- --address="192.168.1.1:9732" &
 pid=$!
 exit_code=$?
 if [[ $exit_code -ne 0 ]]; then
-    sudo ip tuntap del mode tap $interface
+    cleanup
     exit $exit_code
 fi
-trap "kill $pid; sudo ip tuntap del mode tap $interface" INT TERM
-
-
-sudo ip link set $interface up
+trap "kill $pid" INT TERM
 
 wait $pid
-sudo ip tuntap del mode tap $interface
+cleanup
