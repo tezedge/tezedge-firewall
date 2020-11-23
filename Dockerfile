9128bc4c24ca36a1e898d01369d9f1b1979f01d6
Vladislav Melnik <vladislav.melnik@protonmail.com>
2020-11-23 09:52:56 +0000

    prepare for docker build

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+from ubuntu:20.04
+
+# deps
+RUN apt-get update && \
+    DEBIAN_FRONTEND='noninteractive' apt install -y git wget gcc libsodium-dev make zlib1g-dev;
+
+# rust
+ENV RUSTUP_HOME=/usr/local/rustup \
+    CARGO_HOME=/usr/local/cargo \
+    PATH=/usr/local/cargo/bin:$PATH
+RUN set -eux; \
+    url="https://static.rust-lang.org/rustup/dist/x86_64-unknown-linux-gnu/rustup-init"; \
+    wget "$url"; \
+    chmod +x rustup-init; \
+    ./rustup-init -y --no-modify-path --default-toolchain nightly-2020-07-12; \
+    rm rustup-init; \
+    chmod -R a+w $RUSTUP_HOME $CARGO_HOME;
+
+# llvm 11
+RUN wget https://apt.llvm.org/llvm.sh; \
+    chmod +x llvm.sh; \
+    DEBIAN_FRONTEND='noninteractive' apt install -y lsb-release software-properties-common; \
+    ./llvm.sh 11; \
+    rm llvm.sh;
+
+WORKDIR /root
+
+COPY . .
+
+# firewall
+# RUN LLVM_SYS_110_PREFIX=/usr/lib/llvm-11 cargo install --git https://github.com/simplestaking/bpf-firewall.git firewall
+
+# CMD firewall --device eth0
--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use redbpf::{
     HashMap,
     Module,
 };
-use std::{env, ptr, sync::Arc};
+use std::{fs::{self, File}, io::{self, Read}, ptr, sync::Arc, path::PathBuf};
 use tokio::{signal, net::UnixListener, stream::{StreamExt, Stream}, sync::Mutex, io::AsyncReadExt};
 
 #[derive(StructOpt)]
@@ -20,6 +20,8 @@ struct Opts {
         help = "Interface name to attach the firewall"
     )]
     device: String,
+    #[structopt(short, long, default_value = "xdp_module.elf")]
+    probe: PathBuf,
     #[structopt(short, long)]
     block: Vec<String>,
     #[structopt(short, long, default_value = "26.0")]
@@ -83,13 +85,12 @@ where
 
 #[tokio::main]
 async fn main() {
-    let Opts { device, block, target, socket } = Opts::from_args();
+    let Opts { device, probe, block, target, socket } = Opts::from_args();
 
-    let code = include_bytes!(concat!(
-        env!("OUT_DIR"),
-        "/target/bpf/programs/xdp_module/xdp_module.elf"
-    ));
-    let mut loaded = Loader::load(code).expect("error loading BPF program");
+    let mut code = Vec::new();
+    File::open(probe).unwrap().read_to_end(&mut code).unwrap();
+
+    let mut loaded = Loader::load(code.as_ref()).expect("error loading BPF program");
     for kp in loaded.xdps_mut() {
         kp.attach_xdp(device.as_str(), Flags::Unset)
             .expect(&format!("error attaching xdp program {}", kp.name()));
@@ -110,6 +111,15 @@ async fn main() {
     start_event_handler(loaded.events, module.clone(), target);
 
     tokio::spawn(async move {
+        fs::remove_file(socket.clone())
+            .or_else(|e| {
+                if let io::ErrorKind::NotFound = e.kind() {
+                    Ok(())
+                } else {
+                    Err(e)
+                }
+            })
+            .unwrap();
         let mut listener = UnixListener::bind(socket).unwrap();
         loop {
             let (mut stream, _) = listener.accept().await.unwrap();
