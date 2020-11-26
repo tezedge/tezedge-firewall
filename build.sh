4482e9d6ab4e9d3af989f51f1ebc9945b02e0624
Vladislav Melnik <vladislav.melnik@protonmail.com>
2020-11-26 12:12:15 +0000

    minor

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-wget -c https://cdn.kernel.org/pub/linux/kernel/v$2.x/linux-$1.tar.xz
+wget -cq https://cdn.kernel.org/pub/linux/kernel/v$2.x/linux-$1.tar.xz
 tar -xf linux-$1.tar.xz
 pushd linux-$1
 make defconfig
@@ -11,3 +11,4 @@ LLVM_SYS_110_PREFIX=/usr/lib/llvm-11 cargo build -p firewall
 rm -R linux-$1{,.tar.xz}
 mkdir -p target/bpf
 mv target/debug/firewall ../firewall-$1
+cargo clean
