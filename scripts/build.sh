25621ec2278b0058ee25d53fa8e7f019fccb8310
Vladislav Melnik <vladislav.melnik@simplestaking.com>
2020-12-02 11:30:55 +0000

    try use awk

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-export K=${1%-*-*}
+export K=$(awk -F '-' '{print $1}' <<< "$1")
+
 wget -cq https://cdn.kernel.org/pub/linux/kernel/v${K%.*.*}.x/linux-$K.tar.xz
 tar -xf linux-$K.tar.xz
 pushd linux-$K
@@ -14,6 +15,6 @@ export KERNEL_SOURCE=$(pwd)/linux-$K
 cargo build -p tezedge-firewall --bin firewall && cargo build -p tezedge-firewall --bin fw
 rm -R linux-$K{,.tar.xz}
 mkdir -p bin
-mv target/debug/firewall ./bin/firewall-$K
+mv target/debug/firewall ./bin/firewall-$1
 mv target/debug/fw ./bin/fw
 cargo clean
