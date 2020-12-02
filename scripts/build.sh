d043b9bde77671afc49e6a182e580c9aa6b9627a
Vladislav Melnik <vladislav.melnik@simplestaking.com>
2020-12-02 10:48:29 +0000

    try to fix scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,17 +2,18 @@
 
 set -e
 
-wget -cq https://cdn.kernel.org/pub/linux/kernel/v${1%.*.*}.x/linux-$1.tar.xz
-tar -xf linux-$1.tar.xz
-pushd linux-$1
+export K=${1%-*-*}
+wget -cq https://cdn.kernel.org/pub/linux/kernel/v${K%.*.*}.x/linux-$K.tar.xz
+tar -xf linux-$K.tar.xz
+pushd linux-$K
 make defconfig
 make modules_prepare
 popd
-export KERNEL_VERSION=$1
-export KERNEL_SOURCE=$(pwd)/linux-$1
+export KERNEL_VERSION=$K
+export KERNEL_SOURCE=$(pwd)/linux-$K
 cargo build -p tezedge-firewall --bin firewall && cargo build -p tezedge-firewall --bin fw
-rm -R linux-$1{,.tar.xz}
+rm -R linux-$K{,.tar.xz}
 mkdir -p bin
-mv target/debug/firewall ./bin/firewall-$1
+mv target/debug/firewall ./bin/firewall-$K
 mv target/debug/fw ./bin/fw
 cargo clean
