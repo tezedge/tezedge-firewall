5c25d1a0e4e5315c266a9ec7677d0d8af0f0ba6b
Vladislav Melnik <vladislav.melnik@simplestaking.com>
2021-01-29 18:44:26 +0000

    ask sudo password if needed, check kernel version on start

--- tezedge-firewall/src/main.rs
+++ tezedge-firewall/src/main.rs
@@ -1,8 +1,28 @@
 use tezedge_firewall::{firewall, logger};
 use structopt::StructOpt;
+use procfs::sys::kernel::Version;
 
 #[tokio::main]
 async fn main() {
+    // safe to unwrap it never returns error
+    sudo::escalate_if_needed().unwrap();
     let log = logger();
-    firewall(StructOpt::from_args(), log).await
+    match Version::current() {
+        Ok(kernel_version) => {
+            if kernel_version.major < 5 {
+                slog::error!(
+                    log,
+                    "Kernel version is: {:?}, the minimal required version is {:?}",
+                    kernel_version,
+                    Version::new(5, 0, 0),
+                )
+            } else {
+                slog::info!(log, "Kernel version is: {:?}", kernel_version);
+                firewall(StructOpt::from_args(), log).await            
+            }
+        },
+        Err(err) => {
+            slog::error!(log, "Failed to determine kernel version (is it linux?): {:?}", err)
+        },
+    }
 }
